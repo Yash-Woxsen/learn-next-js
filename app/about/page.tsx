005c55a2d19a3fbaf3e465546cb3f9031f9d584b
Yash Office PC <167862397+Yash-Woxsen@users.noreply.github.com>
2025-03-31 17:13:09 +0530

    Init Comit

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const page = () => {
+    return (
+        <div className="text-5xl">About</div>
+    )
+}
+
+export default page;
---
+++ app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,9 @@
+const userPage = ({params}: {params:{id:string}}) => {
+    const {id} = params
+
+    return (
+        <h1 className="text-3xl">User Profile: {id}</h1>
+    );
+}
+
+export default userPage;
